b091153746e3dde16ac3dc8c88a64add94daf49a
Aadit Rao <aaditrao10@gmail.com>
2026-04-04 15:02:28 -0700

    added uniform sampling and gpu sampling

--- src/experiments/targets.py
+++ src/experiments/targets.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cupy as cp
 
 # default is standard normal
 def gaussian_pdf(x, mu=0.0, sigma=1.0):
@@ -19,6 +20,26 @@ def complex_multimodal_pdf(x):
         0.26 * gaussian_pdf(x, mu=-0.4, sigma=0.50) +
         0.31 * gaussian_pdf(x, mu=1.1, sigma=0.65) +
         0.13 * gaussian_pdf(x, mu=3.9, sigma=0.40)
+    )
+
+def gaussian_pdf_cupy(x, mu=0.0, sigma=1.0):
+    return (1.0 / (cp.sqrt(2 * cp.pi) * sigma)) * cp.exp(
+        -0.5 * ((x - mu) / sigma) ** 2
+    )
+
+def bimodal_pdf_cupy(x):
+    return (
+        0.4 * gaussian_pdf_cupy(x, mu=-2.0, sigma=0.7) +
+        0.6 * gaussian_pdf_cupy(x, mu=2.0, sigma=1.0)
+    )
+
+def complex_multimodal_pdf_cupy(x):
+    return (
+        0.12 * gaussian_pdf_cupy(x, mu=-4.7, sigma=0.35) +
+        0.18 * gaussian_pdf_cupy(x, mu=-3.1, sigma=0.90) +
+        0.26 * gaussian_pdf_cupy(x, mu=-0.4, sigma=0.50) +
+        0.31 * gaussian_pdf_cupy(x, mu=1.1, sigma=0.65) +
+        0.13 * gaussian_pdf_cupy(x, mu=3.9, sigma=0.40)
     )
 
 def correlated_gaussian_pdf(X, mu, cov): # multivariate gaussian
